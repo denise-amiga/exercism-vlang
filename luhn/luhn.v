1fd1d6c5618704156548764b45c3a2e4330ecdda
Denise <denise_amiga@hotmail.com>
2024-11-16 12:39:07 +0100

    luhn-iter2

--- luhn/luhn.v
+++ luhn/luhn.v
@@ -1,22 +1,38 @@
 module main
 
-fn valid(value string) bool {
+fn suma[T](a []T) int {
 	mut r := 0
-	mut i := value.len
-	mut p := 0
-	for i > 0 {
-		i--
-		c := value[i] - 48
-		if c == 240 { continue }
-		if c < 0 || c > 9 { return false}
-		p++
+	for c in a {
 		r += c
-		if p % 2 == 0 {
-			r += c
-			if c + c > 9 {
-				r -= 9
+	}
+	return r
+}
+
+fn luhn(mut a []u8) {
+	if a.len > 1 {
+		for i, mut c in a {
+			if i % 2 == 1 {
+				c *= 2
+				if c > 9 {
+					c -= 9
+				}
 			}
 		}
 	}
-	return r % 10 == 0 && p > 1
+}
+
+fn valid(value string) bool {
+	mut n := []u8{}
+	for c in value {
+		if c == ` ` { continue }
+		if c.is_digit() {
+			n << c - 48
+		} else {
+			return false
+		}
+	}
+	if n.len == 1 && n[0] == 0 { return false }
+	n.reverse_in_place()
+	luhn(mut n)
+	return suma(n) % 10 == 0
 }
