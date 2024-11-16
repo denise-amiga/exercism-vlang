3c7caae5b8909f59d8cfd9b9c54b063a1edb8ea4
Denise <denise_amiga@hotmail.com>
2024-11-16 11:35:41 +0100

    prime-factors-iter2

--- prime-factors/prime-factors.v
+++ prime-factors/prime-factors.v
@@ -1,39 +1,20 @@
 module main
 
-import math
-
-fn sieve(limit i64) []i64 {
-	mut res := []i64{}
-	mut s := []bool{len: int(limit + 1), init: true}
-	s[0] = false
-	s[1] = false
-	for i in 1 .. limit + 1 {
-		if s[i] {
-			for j := i * i; j <= limit; j += i {
-				s[j] = false
-			}
-		}
-	}
-	for i, p in s {
-		if p {
-			res << i
-		}
-	}
-	return res
-}
-
 fn prime_factors(n i64) []i64 {
-	mut res := []i64{}
-	mut m := n
-	primes := sieve(int(math.sqrti(n)))
-	for p in primes {
-		for m % p == 0 {
-			res << p
-			m /= p
+	mut primes := []i64{}
+	mut num := n
+	for num % 2 == 0 {
+		primes << 2
+		num >>= 1
+	}
+	mut prime := 3
+	for num != 1 {
+		if num % prime == 0 {
+			primes << prime
+			num /= prime
+			continue
 		}
+		prime += 2
 	}
-	if m > 1 {
-		res << m
-	}
-	return res
+	return primes
 }
