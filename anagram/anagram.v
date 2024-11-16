dc70927c7d3453b8736611331f638d6486898af4
Denise <denise_amiga@hotmail.com>
2024-11-16 11:49:02 +0100

    anagram-iter2

--- anagram/anagram.v
+++ anagram/anagram.v
@@ -2,13 +2,13 @@ module main
 
 fn set(txt string) map[rune]int {
 	mut res := map[rune]int{}
-	for c in txt.runes() {
+	for c in txt.to_lower().runes() {
 		res[c]++
 	}
 	return res
 }
 
 fn find_anagrams(subject string, candidates []string) []string {
-	s := set(subject.to_lower())
-	return candidates.filter(set(it.to_lower()) == s && it.to_lower() != subject.to_lower())
+	s := set(subject)
+	return candidates.filter(set(it) == s && it.to_lower() != subject.to_lower())
 }
